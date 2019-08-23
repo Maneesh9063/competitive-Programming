870ecfccea810148fcf308c31ec953ce2ae7b64b
Maneesh9063 <39021043+Maneesh9063@users.noreply.github.com>
2019-08-23 22:08:08 +0530

    nothing changed test

--- cf568div2_A.py
+++ cf568div2_A.py
@@ -3,8 +3,8 @@ d= a.pop(-1)
 a.sort()
 left = (d-(a[1]-a[0]))
 right = (d-(a[2]-a[1]))
-if left <0:
-	left =0
-if right <0:
-	right =0
+if left < 0:
+	left = 0
+if right < 0:
+	right = 0
 print(left+right)
